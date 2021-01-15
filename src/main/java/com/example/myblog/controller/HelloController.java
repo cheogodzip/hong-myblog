f5799c32febfebeb0775fe9fe53e49952f41153a
cheogodzip <lkver@naver.com>
2021-01-15 20:18:36 +0900

    스프링 공부 첫 커밋

--- src/main/java/com/example/myblog/controller/HelloController.java
+++ src/main/java/com/example/myblog/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.example.myblog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller// 해당 클래스를, 컨트롤러로 선언!
+@Controller//  해당 클래스를, 컨트롤러로 선언!
 public class HelloController {
 
     @GetMapping("/") // 다음 url 요청을 받음 -> "/"를 보여줘 -> 아래 메소드를 수행!
