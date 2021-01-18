4db1447b61eb2787bfaf84356cdfa6980c6746fe
cheogodzip <lkver@naver.com>
2021-01-18 19:26:10 +0900

    다른 페이지 추가 / 유튜브 강의

--- src/main/java/com/example/myblog/controller/HelloController.java
+++ src/main/java/com/example/myblog/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.myblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller//  해당 클래스를, 컨트롤러로 선언!
@@ -9,6 +10,18 @@ public class HelloController {
     @GetMapping("/") // 다음 url 요청을 받음 -> "/"를 보여줘 -> 아래 메소드를 수행!
     public String hello() {
         return "helloworld"; // helloworld.mustache 페이지를 반환!
+    }
+
+    @GetMapping("/hi")
+    public String hi(Model model){
+        model.addAttribute("username", "Shinosuke");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname", "신짱구");
+        return "goodbye";
     }
 
     @GetMapping("/greetings")
