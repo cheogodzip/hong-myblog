fb209b5569e1d6cd28e907a5a20b243e97081ff3
cheogodzip <lkver@naver.com>
2021-01-21 10:02:05 +0900

    db에 담긴 데이터를 볼 수 있도록 수정해봄

--- src/main/java/com/example/myblog/controller/ArticleController.java
+++ src/main/java/com/example/myblog/controller/ArticleController.java
@@ -7,7 +7,9 @@ import com.example.myblog.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 컨트롤러 선언
@@ -39,5 +41,21 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id: " + id);
+
+        //1. id로 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //id 값을 통해 찾았는데 값이 없다면 null을 반환해라 라는 뜻
+
+        //2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+
+        //3. 보여줄 페이지 설정
+        return "articles/show";
     }
 }
--- src/main/java/com/example/myblog/entity/Article.java
+++ src/main/java/com/example/myblog/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.myblog.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor //디폴트 생성자
 @ToString
 public class Article {
 
